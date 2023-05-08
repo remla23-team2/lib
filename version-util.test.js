84801c90b4ac9adf52a6ed917ccea8f8eca26d4f
Penzhi <tyypz2590477658@gmail.com>
2023-05-08 23:42:01 +0200

    I think I made it

--- version-util.test.js
+++ version-util.test.js
@@ -1,7 +1,7 @@
 const VersionUtil = require('./version-util');
 
 test('getVersion returns the correct version', () => {
-  expect(VersionUtil.getVersion()).toBe('1.0.74');
+  expect(VersionUtil.getVersion()).toBe('1.0.86');
 });
 
 console.log('VersionUtil.getVersion() =', VersionUtil.getVersion());

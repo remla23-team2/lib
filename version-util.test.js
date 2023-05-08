491bd65c003cf04899db533e9f9b5726f74b5c0f
Penzhi <tyypz2590477658@gmail.com>
2023-05-08 22:43:08 +0200

    test

--- version-util.test.js
+++ version-util.test.js
@@ -3,3 +3,5 @@ const VersionUtil = require('./version-util');
 test('getVersion returns the correct version', () => {
   expect(VersionUtil.getVersion()).toBe('1.0.74');
 });
+
+console.log('VersionUtil.getVersion() =', VersionUtil.getVersion());

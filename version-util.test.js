f34b3f7606e9289547c9a9137eb2af6808a0a3b9
Merlijn Mac Gillavry <merlijnmac@gmail.com>
2023-05-11 19:51:04 +0200

    set right test version

--- version-util.test.js
+++ version-util.test.js
@@ -1,7 +1,7 @@
 const VersionUtil = require('./version-util');
 
 test('getVersion returns the correct version', () => {
-  expect(VersionUtil.getVersion()).toBe('1.0.6');
+  expect(VersionUtil.getVersion()).toBe('1.0.8');
 });
 
 console.log('VersionUtil.getVersion() =', VersionUtil.getVersion());

f7bc181bbeace8aa3af84300e3a5ec91ace234db
Merlijn Mac Gillavry <merlijnmac@gmail.com>
2023-05-11 18:43:20 +0200

    updated test file

--- version-util.test.js
+++ version-util.test.js
@@ -1,7 +1,7 @@
 const VersionUtil = require('./version-util');
 
 test('getVersion returns the correct version', () => {
-  expect(VersionUtil.getVersion()).toBe('1.0.86');
+  expect(VersionUtil.getVersion()).toBe('1.0.88');
 });
 
 console.log('VersionUtil.getVersion() =', VersionUtil.getVersion());

4d21f05daf4d5575c60130fc383e6beae9de1bbe
Merlijn Mac Gillavry <merlijnmac@gmail.com>
2023-05-12 16:19:45 +0200

    changed library name

--- version-util.test.js
+++ version-util.test.js
@@ -1,7 +1,7 @@
 const VersionUtil = require('./version-util');
 
 test('getVersion returns the correct version', () => {
-  expect(VersionUtil.getVersion()).toBe('1.0.11');
+  expect(VersionUtil.getVersion()).toBe('1.0.12');
 });
 
 console.log('VersionUtil.getVersion() =', VersionUtil.getVersion());
